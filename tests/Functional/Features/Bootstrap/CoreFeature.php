40cd20b421c0cc23951d8c4fbb0088bb1d94439a
Michel <michelpl@yahoo.com.br>
2019-05-10 23:14:37 -0300

    :green_heart: Add tests do CI.

--- tests/Functional/Features/Bootstrap/CoreFeature.php
+++ tests/Functional/Features/Bootstrap/CoreFeature.php
@@ -19,4 +19,60 @@ class CoreFeature extends MinkContext
     protected $scenarioTokens = null;
     protected static $featureHash = null;
     protected $screenshotDir = DIRECTORY_SEPARATOR . 'tmp';
+
+    /**
+     *
+     * @AfterStep
+     * @param     $event
+     */
+    protected function afterStepFailureScreenshot($event)
+    {
+        $e = $event->getTestResult()->getCallResult()->getException();
+        if ($e) {
+            if (!file_exists($this->screenshotDir)) {
+                mkdir($this->screenshotDir);
+            }
+            $filename = tempnam($this->screenshotDir, "failure_screenshoot_");
+            unlink($filename);
+            $filename .= ".png";
+            $this->screenshot($filename);
+            echo "saved failure screenshot to '$filename'";
+        }
+    }
+
+    /**
+     *
+     * @BeforeFeature
+     */
+    protected static function beforeFeature($event)
+    {
+        self::$featureHash = null;
+        $requestTime = $_SERVER['REQUEST_TIME'];
+        $featureTitle = $event->getFeature()->getTitle();
+        $hash = hash('sha512', $featureTitle . $requestTime);
+        self::$featureHash = substr($hash, 0, 16);
+    }
+
+
+    /**
+     *
+     * @BeforeScenario
+     */
+    protected function beforeScenario($event)
+    {
+        if ($event->getScenario()->hasTag('smartStep')) {
+            /*throw new PendingException(
+                'This is a partial @smartStep Scenario and should not be isolatedly executed.'
+            );*/
+        }
+
+        $this->scenarioTokens = null;
+        try {
+            //trying to save examples to use in @smartStep
+            $this->scenarioTokens =
+                $event->getScenario()->getTokens();
+        }catch(Throwable $e) {
+        }
+    }
+
 }
