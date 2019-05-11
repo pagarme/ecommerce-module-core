3026a03e6334370602160102d9aa253e9dace9e3
Michel <michelpl@yahoo.com.br>
2019-05-11 00:38:32 -0300

    :green_heart: Add tests do CI.

--- tests/Functional/Features/Bootstrap/CoreFeature.php
+++ tests/Functional/Features/Bootstrap/CoreFeature.php
@@ -472,4 +472,36 @@ class CoreFeature extends MinkContext
 
 
 
+    /**
+     *
+     * @Given /^a new session$/
+     */
+    public function newSession()
+    {
+        $this->getSession()->reset();
+        //throw new Exception("as");
+    }
+
+    /**
+     *
+     * @Given /^I define failure screenshot dir as "([^"]*)"$/
+     */
+    public function setScreenshotDir($dir)
+    {
+        $this->screenshotDir = $dir;
+    }
+
+    /**
+     *
+     * @Given /^I save a screenshot to "([^"]*)" file$/
+     */
+    public function screenshot($filename)
+    {
+        $driver =  $this->getSession()->getDriver();
+        $data = $driver->getScreenshot();
+        $file = fopen($filename, "w");
+        fwrite($file, $data);
+        fclose($file);
+    }
+
 }
