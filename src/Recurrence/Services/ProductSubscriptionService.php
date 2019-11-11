7d1b9595d7063036e78deafa5fc0714233be17a5
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2019-11-11 11:49:37 -0300

    :loud_sound: Added log on product subscription service

--- src/Recurrence/Services/ProductSubscriptionService.php
+++ src/Recurrence/Services/ProductSubscriptionService.php
@@ -2,19 +2,34 @@
 
 namespace Mundipagg\Core\Recurrence\Services;
 
+use Mundipagg\Core\Kernel\Services\LogService;
 use Mundipagg\Core\Recurrence\Factories\ProductSubscriptionFactory;
 use Mundipagg\Core\Recurrence\Repositories\ProductSubscriptionRepository;
 
 class ProductSubscriptionService
 {
+    /** @var LogService  */
+    protected $logService;
+
+    public function __construct()
+    {
+        $this->logService = new LogService(
+            'ProductSubscriptionService',
+            true
+        );
+    }
+
     public function createAtPlatform($formData)
     {
+        $this->logService->info("Creating product subscription at platform");
         $productSubscriptionFactory = new ProductSubscriptionFactory();
         $productSubscription = $productSubscriptionFactory->createFromPostData($formData);
 
-         $productSubscriptionRepository = new ProductSubscriptionRepository();
-         $productSubscriptionRepository->save($productSubscription);
-         return $productSubscription;
+        $productSubscriptionRepository = new ProductSubscriptionRepository();
+        $productSubscriptionRepository->save($productSubscription);
+        $this->logService->info("Subscription created: " . $productSubscription->getId());
+
+        return $productSubscription;
     }
 
     public function findById($id)
