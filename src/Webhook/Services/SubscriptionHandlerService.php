35d496f46f4bc4c44a690ac53b922fe081a30c63
Wallace Ferreira <wallace.sf87@gmail.com>
2019-11-19 17:10:55 -0200

    :zap: creating webhook canceled to subscription

--- src/Webhook/Services/SubscriptionHandlerService.php
+++ src/Webhook/Services/SubscriptionHandlerService.php
@@ -8,10 +8,14 @@ use Mundipagg\Core\Kernel\Exceptions\NotFoundException;
 use Mundipagg\Core\Kernel\Factories\OrderFactory;
 use Mundipagg\Core\Kernel\Interfaces\PlatformOrderInterface;
 use Mundipagg\Core\Kernel\Repositories\OrderRepository;
+use Mundipagg\Core\Kernel\Services\LocalizationService;
+use Mundipagg\Core\Kernel\Services\OrderService;
 use Mundipagg\Core\Webhook\Aggregates\Webhook;
+use Mundipagg\Core\Recurrence\Aggregates\Subscription;
 
 class SubscriptionHandlerService extends AbstractHandlerService
 {
+
     protected function handleCreated(Webhook $webhook)
     {
         throw new \Exception('Not implemented');
@@ -19,31 +23,55 @@ class SubscriptionHandlerService extends AbstractHandlerService
 
     protected function handleCanceled(Webhook $webhook)
     {
-        throw new \Exception('Not implemented canceled');
+        $subscriptionRepository = new \Mundipagg\Core\Recurrence\Repositories\SubscriptionRepository();
+        $orderService = new OrderService();
+        $i18n = new LocalizationService();
+
+        /**
+         * @var Subscription
+         */
+        $subscription = $webhook->getEntity();
+
+        $outdatedSubscription = $subscriptionRepository->findByMundipaggId($subscription->getMundipaggId());
+        if ($outdatedSubscription != null) {
+            $outdatedSubscription->setStatus($subscription->getStatus());
+            $subscription = $outdatedSubscription;
+        }
+
+        $subscriptionRepository->save($subscription);
+
+        $history = $i18n->getDashboard('Subscription canceled');
+        $this->order->getPlatformOrder()->addHistoryComment($history);
+
+        $orderService->syncPlatformWith($this->order);
+
+        $result = [
+            "message" => 'Subscription cancel registered',
+            "code" => 200
+        ];
+
+        return $result;
     }
 
     protected function loadOrder(Webhook $webhook)
     {
         $orderRepository = new OrderRepository();
+
         /**
-         *
          * @var Order $order
          */
         $order = $webhook->getEntity();
        // $order = $orderRepository->findByMundipaggId($order->getMundipaggId());
         $order = $orderRepository->findByCode($order->getCode());
         if ($order === null) {
-
-            $orderDecoratorClass =
-                MPSetup::get(MPSetup::CONCRETE_PLATFORM_ORDER_DECORATOR_CLASS);
+            $orderDecoratorClass = MPSetup::get(MPSetup::CONCRETE_PLATFORM_ORDER_DECORATOR_CLASS);
 
             /**
-             *
              * @var Order $webhookOrder
              */
             $webhookOrder = $webhook->getEntity();
+
             /**
-             *
              * @var PlatformOrderInterface $order
              */
             $order = new $orderDecoratorClass();
