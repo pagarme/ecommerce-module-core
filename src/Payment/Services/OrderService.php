4eb5e9aba3da0aa1372dc3606b2e4ce86353e53c
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2020-11-12 19:47:00 -0300

    :art: Add payment order service

---
+++ src/Payment/Services/OrderService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Mundipagg\Core\Payment\Services;
+
+use Mundipagg\Core\Kernel\Repositories\OrderRepository;
+use Mundipagg\Core\Kernel\ValueObjects\Id\OrderId;
+
+class OrderService
+{
+    public function getPixQrCodeInfoFromOrder(OrderId $orderId)
+    {
+        $orderRepository = new OrderRepository();
+        $order = $orderRepository->findByMundipaggId(new OrderId($orderId));
+        $qrCodeInfo = [];
+
+        if ($order !== null) {
+            $charges = $order->getCharges();
+            foreach ($charges as $charge) {
+                $transaction = $charge->getLastTransaction();
+                $postData = $transaction->getPostData();
+                $data = json_decode($postData->tran_data, true);
+                if (!empty($data['qr_code']) && !empty($data['qr_code_url'])) {
+                    $qrCodeInfo['qr_code'] = $data['qr_code'];
+                    $qrCodeInfo['qr_code_url'] = $data['qr_code_url'];
+                }
+            }
+        }
+
+        return $qrCodeInfo;
+    }
+}
