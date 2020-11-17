cb0ff3584a9b2e412799dc5c331968d7df357973
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2020-11-17 20:20:53 -0300

    :ok_hand: Code Review

--- src/Payment/Services/OrderService.php
+++ src/Payment/Services/OrderService.php
@@ -16,16 +16,24 @@ class OrderService
         if ($order !== null) {
             $charges = $order->getCharges();
             foreach ($charges as $charge) {
-                $transaction = $charge->getLastTransaction();
-                $postData = $transaction->getPostData();
-                $data = json_decode($postData->tran_data, true);
-                if (!empty($data['qr_code']) && !empty($data['qr_code_url'])) {
-                    $qrCodeInfo['qr_code'] = $data['qr_code'];
-                    $qrCodeInfo['qr_code_url'] = $data['qr_code_url'];
-                }
+                $qrCodeInfo = $this->getInfoFromCharge($charge);
             }
         }
 
         return $qrCodeInfo;
+    }
+
+    private function getInfoFromCharge($charge)
+    {
+        $transaction = $charge->getLastTransaction();
+        $postData = $transaction->getPostData();
+        $data = json_decode($postData->tran_data, true);
+        if (!empty($data['qr_code']) && !empty($data['qr_code_url'])) {
+            $qrCodeInfo['qr_code'] = $data['qr_code'];
+            $qrCodeInfo['qr_code_url'] = $data['qr_code_url'];
+            return $qrCodeInfo;
+        }
+
+        return [];
     }
 }
