e7b6cc55b3842869026f26388fddd1020e954995
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2020-12-02 11:58:15 -0300

    :bug: Fix saved card on subscription

--- src/Payment/Services/CardService.php
+++ src/Payment/Services/CardService.php
@@ -3,10 +3,22 @@
 namespace Mundipagg\Core\Payment\Services;
 
 use Mundipagg\Core\Kernel\Abstractions\AbstractEntity;
+use Mundipagg\Core\Kernel\Aggregates\Order;
+use Mundipagg\Core\Kernel\Services\LogService;
 use Mundipagg\Core\Kernel\ValueObjects\CardBrand;
+use Mundipagg\Core\Kernel\ValueObjects\TransactionType;
+use Mundipagg\Core\Payment\Factories\SavedCardFactory;
+use Mundipagg\Core\Payment\Repositories\SavedCardRepository;
 
 class CardService
 {
+    private $logService;
+
+    public function __construct()
+    {
+        $this->logService = $this->getLogService();
+    }
+
     public function getBrandsAvailables(AbstractEntity $config)
     {
         $brandsAvailables = [];
@@ -22,5 +34,64 @@ class CardService
         }
 
         return $brandsAvailables;
+    }
+
+    public function saveCards(Order $order)
+    {
+        $savedCardFactory = new SavedCardFactory();
+        $savedCardRepository = new SavedCardRepository();
+        $charges = $order->getCharges();
+
+        foreach ($charges as $charge) {
+            $lastTransaction = $charge->getLastTransaction();
+            if ($lastTransaction === null) {
+                continue;
+            }
+
+            if (
+            !(
+                $lastTransaction->getTransactionType()->equals(TransactionType::creditCard()) ||
+                $lastTransaction->getTransactionType()->equals(TransactionType::voucher()) ||
+                $lastTransaction->getTransactionType()->equals(TransactionType::debitCard())
+            )
+            ) {
+                continue; //save only credit card transactions;
+            }
+
+            $metadata = $charge->getMetadata();
+            $saveOnSuccess =
+                isset($metadata->saveOnSuccess) &&
+                $metadata->saveOnSuccess === "true";
+
+            if (
+                !empty($lastTransaction->getCardData()) &&
+                $saveOnSuccess &&
+                $order->getCustomer()->getMundipaggId()->equals(
+                    $charge->getCustomer()->getMundipaggId()
+                )
+            ) {
+                $postData =
+                    json_decode(json_encode($lastTransaction->getCardData()));
+                $postData->owner =
+                    $charge->getCustomer()->getMundipaggId();
+
+                $savedCard = $savedCardFactory->createFromTransactionJson($postData);
+                if (
+                    $savedCardRepository->findByMundipaggId($savedCard->getMundipaggId()) === null
+                ) {
+                    $savedCardRepository->save($savedCard);
+                    $this->logService->info(
+                        $order->getCode(),
+                        "Card '{$savedCard->getMundipaggId()->getValue()}' saved."
+                    );
+
+                }
+            }
+        }
+    }
+
+    public function getLogService()
+    {
+        return new LogService("Card Service", true);
     }
 }
