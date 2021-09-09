7018bdc3e02a30cfd807b9edf0f645959e5a2d8c
Yan Pantoja <yanmpantoja@gmail.com>
2021-09-09 20:07:40 -0300

    :recycle: removes interface and adds null array check

--- src/Marketplace/Interfaces/ConvertibleToSDKRequestsInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Pagarme\Core\Marketplace\Interfaces;
-
-interface ConvertibleToSDKRequestsInterface
-{
-    public function convertMainToSDKRequest();
-    public function convertSecondaryToSDKRequest();
-}
