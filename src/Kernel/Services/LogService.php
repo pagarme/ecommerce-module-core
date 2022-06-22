9a2cf85fc492a40201310f225bcd62b3b7151f87
M3ndes <thalesmendes.dev@gmail.com>
2022-06-22 16:02:11 -0300

    added method to blur charge customer object

--- src/Kernel/Services/LogService.php
+++ src/Kernel/Services/LogService.php
@@ -99,43 +99,75 @@ class LogService
     {
         if (isset($data['data'])) {
             if (isset($data['data']['customer'])) {
-                $data['data']['customer']['name'] = preg_replace('/^.{8}/', '$1**', $data['data']['customer']['name']);
-                $data['data']['customer']['email'] = preg_replace('/\B[^@.]/', '*', $data['data']['customer']['email']);
-                $data['data']['customer']['document'] = preg_replace('/\B[^@.]/', '*', $data['data']['customer']['document']);
-                $data['data']['customer']['phones'] = null;
-                $data['data']['customer']['address']['street'] = preg_replace('/^.{8}/', '$1**', $data['data']['customer']['address']['street']);
-                $data['data']['customer']['address']['line_1'] = preg_replace('/^.{8}/', '$1**', $data['data']['customer']['address']['line_1']);
-                $data['data']['customer']['address']['line_2'] = null;
-                $data['data']['customer']['address']['number'] = null;
-                $data['data']['customer']['address']['complement'] = null;
-                $data['data']['customer']['address']['zip_code'] = preg_replace('/^.{5}/', '$1**', $data['data']['customer']['address']['zip_code']);
-                $data['data']['customer']['address']['neighborhood'] = null;
+                $data = $this->blurCustomer($data);
             }
             if (isset($data['data']['shipping'])) {
-                $data['data']['shipping']['recipient_name'] = preg_replace('/^.{8}/', '$1**', $data['data']['shipping']['recipient_name']);
-                $data['data']['shipping']['recipient_phone'] = null;
-                $data['data']['shipping']['address']['street'] = preg_replace('/^.{8}/', '$1**',  $data['data']['shipping']['address']['street']);
-                $data['data']['shipping']['address']['line_1'] = preg_replace('/^.{8}/', '$1**', $data['data']['shipping']['address']['line_1']);
-                $data['data']['shipping']['address']['line_2'] = null;
-                $data['data']['shipping']['address']['number'] = null;
-                $data['data']['shipping']['address']['complement'] = null;
-                $data['data']['shipping']['address']['zip_code'] = preg_replace('/^.{5}/', '$1**', $data['data']['shipping']['address']['zip_code']);
-                $data['data']['shipping']['address']['neighborhood'] = null;
+                $data = $this->blurShipping($data);
             }
             if (isset($data['data']['payments'][0]['customer'])) {
-                $data['data']['payments'][0]['customer']['name'] = preg_replace('/^.{8}/', '$1**', $data['data']['payments'][0]['customer']['name']);
-                $data['data']['payments'][0]['customer']['email'] = preg_replace('/\B[^@.]/', '*', $data['data']['payments'][0]['customer']['email']);
-                $data['data']['payments'][0]['customer']['document'] = preg_replace('/\B[^@.]/', '*', $data['data']['payments'][0]['customer']['document']);
-                $data['data']['payments'][0]['customer']['phones'] = null;
-                $data['data']['payments'][0]['customer']['address']['street'] = preg_replace('/^.{8}/', '$1**', $data['data']['payments'][0]['customer']['address']['street']);
-                $data['data']['payments'][0]['customer']['address']['line_1'] = preg_replace('/^.{8}/', '$1**', $data['data']['payments'][0]['customer']['address']['line_1']);
-                $data['data']['payments'][0]['customer']['address']['line_2'] = null;
-                $data['data']['payments'][0]['customer']['address']['number'] = null;
-                $data['data']['payments'][0]['customer']['address']['complement'] = null;
-                $data['data']['payments'][0]['customer']['address']['zip_code'] = preg_replace('/^.{5}/', '$1**', $data['data']['payments'][0]['customer']['address']['zip_code']);
-                $data['data']['payments'][0]['customer']['address']['neighborhood'] = null;
+                $data = $this->blurPaymentCustomer($data);
+            }
+            if (isset($data['data']['charges'][0]['customer'])) {
+                $data = $this->blurChargeCustomer($data);
             }
         }
+        return $data;
+    }
+
+    private function blurCustomer($data){
+        $data['data']['customer']['name'] = preg_replace('/^.{8}/', '$1**', $data['data']['customer']['name']);
+        $data['data']['customer']['email'] = preg_replace('/\B[^@.]/', '*', $data['data']['customer']['email']);
+        $data['data']['customer']['document'] = preg_replace('/\B[^@.]/', '*', $data['data']['customer']['document']);
+        $data['data']['customer']['phones'] = null;
+        $data['data']['customer']['address']['street'] = preg_replace('/^.{8}/', '$1**', $data['data']['customer']['address']['street']);
+        $data['data']['customer']['address']['line_1'] = preg_replace('/^.{8}/', '$1**', $data['data']['customer']['address']['line_1']);
+        $data['data']['customer']['address']['line_2'] = null;
+        $data['data']['customer']['address']['number'] = null;
+        $data['data']['customer']['address']['complement'] = null;
+        $data['data']['customer']['address']['zip_code'] = preg_replace('/^.{5}/', '$1**', $data['data']['customer']['address']['zip_code']);
+        $data['data']['customer']['address']['neighborhood'] = null;
+        return $data;
+    }
+
+    private function blurShipping($data){
+        $data['data']['shipping']['recipient_name'] = preg_replace('/^.{8}/', '$1**', $data['data']['shipping']['recipient_name']);
+        $data['data']['shipping']['recipient_phone'] = null;
+        $data['data']['shipping']['address']['street'] = preg_replace('/^.{8}/', '$1**',  $data['data']['shipping']['address']['street']);
+        $data['data']['shipping']['address']['line_1'] = preg_replace('/^.{8}/', '$1**', $data['data']['shipping']['address']['line_1']);
+        $data['data']['shipping']['address']['line_2'] = null;
+        $data['data']['shipping']['address']['number'] = null;
+        $data['data']['shipping']['address']['complement'] = null;
+        $data['data']['shipping']['address']['zip_code'] = preg_replace('/^.{5}/', '$1**', $data['data']['shipping']['address']['zip_code']);
+        $data['data']['shipping']['address']['neighborhood'] = null;
+        return $data;
+    }
+
+    private function blurPaymentCustomer($data){
+        $data['data']['payments'][0]['customer']['name'] = preg_replace('/^.{8}/', '$1**', $data['data']['payments'][0]['customer']['name']);
+        $data['data']['payments'][0]['customer']['email'] = preg_replace('/\B[^@.]/', '*', $data['data']['payments'][0]['customer']['email']);
+        $data['data']['payments'][0]['customer']['document'] = preg_replace('/\B[^@.]/', '*', $data['data']['payments'][0]['customer']['document']);
+        $data['data']['payments'][0]['customer']['phones'] = null;
+        $data['data']['payments'][0]['customer']['address']['street'] = preg_replace('/^.{8}/', '$1**', $data['data']['payments'][0]['customer']['address']['street']);
+        $data['data']['payments'][0]['customer']['address']['line_1'] = preg_replace('/^.{8}/', '$1**', $data['data']['payments'][0]['customer']['address']['line_1']);
+        $data['data']['payments'][0]['customer']['address']['line_2'] = null;
+        $data['data']['payments'][0]['customer']['address']['number'] = null;
+        $data['data']['payments'][0]['customer']['address']['complement'] = null;
+        $data['data']['payments'][0]['customer']['address']['zip_code'] = preg_replace('/^.{5}/', '$1**', $data['data']['payments'][0]['customer']['address']['zip_code']);
+        $data['data']['payments'][0]['customer']['address']['neighborhood'] = null;
+        return $data;
+    }
+    private function blurChargeCustomer($data){
+        $data['data']['charges'][0]['customer']['name'] = preg_replace('/^.{8}/', '$1**', $data['data']['payments'][0]['customer']['name']);
+        $data['data']['charges'][0]['customer']['email'] = preg_replace('/\B[^@.]/', '*', $data['data']['payments'][0]['customer']['email']);
+        $data['data']['charges'][0]['customer']['document'] = preg_replace('/\B[^@.]/', '*', $data['data']['payments'][0]['customer']['document']);
+        $data['data']['charges'][0]['customer']['phones'] = null;
+        $data['data']['charges'][0]['customer']['address']['street'] = preg_replace('/^.{8}/', '$1**', $data['data']['payments'][0]['customer']['address']['street']);
+        $data['data']['charges'][0]['customer']['address']['line_1'] = preg_replace('/^.{8}/', '$1**', $data['data']['payments'][0]['customer']['address']['line_1']);
+        $data['data']['charges'][0]['customer']['address']['line_2'] = null;
+        $data['data']['charges'][0]['customer']['address']['number'] = null;
+        $data['data']['charges'][0]['customer']['address']['complement'] = null;
+        $data['data']['charges'][0]['customer']['address']['zip_code'] = preg_replace('/^.{5}/', '$1**', $data['data']['payments'][0]['customer']['address']['zip_code']);
+        $data['data']['charges'][0]['customer']['address']['neighborhood'] = null;
         return $data;
     }
 }
