3de891de7a5fed141c47e700ec4853369a61ea87
Fabiano Mallmann <25328512+fabiano-mallmann@users.noreply.github.com>
2023-04-05 17:01:26 -0300

    fix conflict bug and add php8 compatibility

--- src/Payment/Aggregates/Order.php
+++ src/Payment/Aggregates/Order.php
@@ -301,13 +301,6 @@ final class Order extends AbstractEntity implements ConvertibleToSDKRequestsInte
      * @return mixed data which can be serialized by <b>json_encode</b>,
      * which is a value of any type other than a resource.
      * @since 5.4.0
-     */
-
-      * Specify data which should be serialized to JSON
-      * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
-      * @return mixed data which can be serialized by <b>json_encode</b>,
-      * which is a value of any type other than a resource.
-      * @since 5.4.0
     */
     #[\ReturnTypeWillChange]
 
