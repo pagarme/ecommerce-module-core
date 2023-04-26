d74f2f5f55582e8bc9cad3f2dc712176f90f5208
RafaMelazzo <rafael.melazzo@stone.com.br>
2023-04-26 15:10:28 -0300

    Fixing deprecated "Passing null to parameter"

--- src/Payment/Aggregates/Payments/AbstractPayment.php
+++ src/Payment/Aggregates/Payments/AbstractPayment.php
@@ -121,7 +121,7 @@ implements ConvertibleToSDKRequestsInterface, HaveOrderInterface
 
     private function cammel2SnakeCase($cammelCaseString)
     {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $cammelCaseString, $matches);
+        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $cammelCaseString ?? '', $matches);
         $ret = $matches[0];
         foreach ($ret as &$match) {
             $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
