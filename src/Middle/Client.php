c871b6f4eb42ea34961c739eacb159c116522d45
Mateus Picoloto <mateus.picoloto@pagar.me>
2023-07-05 09:56:00 -0300

    adding card connection and recurrence cycle information

---
+++ src/Middle/Client.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Pagarme\Core\Middle;
+
+use PagarmeCoreApiLib\PagarmeCoreApiClient;
+use PagarmeCoreApiLib\Configuration;
+
+/**
+ * This class is responsible for authentication.
+ */
+abstract class Client 
+{
+    /** @var PagarmeCoreApiClient  */
+    public $client;
+
+    abstract public function getHubToken();
+    public function __construct()
+    {
+        Configuration::$basicAuthPassword = '';
+        $this->client = $this->services();
+    }
+    private function auth()
+    {
+        return new PagarmeCoreApiClient($this->getHubToken(), "");
+    }
+
+    public function services()
+    {
+        return $this->auth();
+    }
+
+}
