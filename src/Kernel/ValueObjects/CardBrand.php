f896b3cb39a38f44c27ac5a4461a208b1ff2dadb
RafaMelazzo <rafael.melazzo@stone.com.br>
2024-04-23 12:10:06 -0300

    feat: add ticket brand to voucher payment

--- src/Kernel/ValueObjects/CardBrand.php
+++ src/Kernel/ValueObjects/CardBrand.php
@@ -3,34 +3,36 @@
 namespace Pagarme\Core\Kernel\ValueObjects;
 
 use Pagarme\Core\Kernel\Abstractions\AbstractValueObject;
+use ReturnTypeWillChange;
 
 /** @todo there is a way to make the enumeration behavior of classes like this abstract.
  */
 final class CardBrand extends AbstractValueObject
 {
-    CONST NO_BRAND = "noBrand";
+    const NO_BRAND = "noBrand";
 
-    CONST VISA = "Visa";
-    CONST MASTERCARD = "Mastercard";
-    CONST AMEX = "Amex";
-    CONST HIPERCARD = "HiperCard";
-    CONST DINERS = "Diners";
-    CONST ELO = "Elo";
-    CONST DISCOVER = "Discover";
-    CONST AURA = "Aura";
-    CONST JCB = "JCB";
-    CONST CREDZ = "Credz";
-    CONST SODEXO_ALIMENTACAO = "SodexoAlimentacao";
-    CONST SODEXO_CULTURA = "SodexoCultura";
-    CONST SODEXO_GIFT = "SodexoGift";
-    CONST SODEXO_PREMIUM = "SodexoPremium";
-    CONST SODEXO_REFEICAO = "SodexoRefeicao";
-    CONST SODEXO_COMBUSTIVEL = "SodexoCombustivel";
-    CONST VR = "VR";
-    CONST ALELO = "Alelo";
-    CONST BANESE = "Banese";
-    CONST CABAL = "Cabal";
-    CONST SODEXO = "Sodexo";
+    const VISA = "Visa";
+    const MASTERCARD = "Mastercard";
+    const AMEX = "Amex";
+    const HIPERCARD = "HiperCard";
+    const DINERS = "Diners";
+    const ELO = "Elo";
+    const DISCOVER = "Discover";
+    const AURA = "Aura";
+    const JCB = "JCB";
+    const CREDZ = "Credz";
+    const SODEXO_ALIMENTACAO = "SodexoAlimentacao";
+    const SODEXO_CULTURA = "SodexoCultura";
+    const SODEXO_GIFT = "SodexoGift";
+    const SODEXO_PREMIUM = "SodexoPremium";
+    const SODEXO_REFEICAO = "SodexoRefeicao";
+    const SODEXO_COMBUSTIVEL = "SodexoCombustivel";
+    const VR = "VR";
+    const ALELO = "Alelo";
+    const BANESE = "Banese";
+    const CABAL = "Cabal";
+    const SODEXO = "Sodexo";
+    const TICKET = "Ticket";
 
     private $name;
 
@@ -42,11 +44,6 @@ final class CardBrand extends AbstractValueObject
     private function setName($name)
     {
         $this->name = $name;
-    }
-
-    public function getName()
-    {
-        return $this->name;
     }
 
     static public function nobrand()
@@ -159,11 +156,30 @@ final class CardBrand extends AbstractValueObject
         return new self(self::SODEXO);
     }
 
+    static public function ticket()
+    {
+        return new self(self::TICKET);
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     *
+     * @link   https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since  5.4.0
+     */
+    #[ReturnTypeWillChange]
+    public function jsonSerialize()
+    {
+        return $this->getName();
+    }
+
     /**
      * To check the structural equality of value objects,
      * this method should be implemented in this class children.
      *
-     * @param  CardBrand $object
+     * @param CardBrand $object
      * @return bool
      */
     protected function isEqual($object)
@@ -171,17 +187,8 @@ final class CardBrand extends AbstractValueObject
         return $this->getName() === $object->getName();
     }
 
-    /**
-      * Specify data which should be serialized to JSON
-      *
-      * @link   https://php.net/manual/en/jsonserializable.jsonserialize.php
-      * @return mixed data which can be serialized by <b>json_encode</b>,
-      * which is a value of any type other than a resource.
-      * @since  5.4.0
-    */
-    #[\ReturnTypeWillChange]
-    public function jsonSerialize()
+    public function getName()
     {
-        return $this->getName();
+        return $this->name;
     }
 }
